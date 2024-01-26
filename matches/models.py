e49630059c460677204cb9716b9995d8a8c7ea7c
Mohamadhosein Moazennia <mhmoazennia@gmail.com>
2024-01-26 20:17:06 +0330

    feat: Create Match and Seat models

--- matches/models.py
+++ matches/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Match(models.Model):
+    stadium = models.ForeignKey("stadiums.Stadium", on_delete=models.PROTECT)
+    home_side = models.CharField(max_length=50)
+    away_side = models.CharField(max_length=50)
+    match_day = models.DateField(auto_now=False, auto_now_add=False)
+    match_time = models.TimeField(auto_now=False, auto_now_add=False)
+
+    class Meta:
+        verbose_name = "match"
+        verbose_name_plural = "matches"
+        unique_together = ["stadium", "match_day", "match_time"]
+
+    def __str__(self):
+        return (
+            f"{self.home_side} vs {self.away_side} on {self.datetime} at {self.stadium}"
+        )
+
+
+class Seat(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    seat_number = models.IntegerField()
+    is_reserved = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "seat"
+        verbose_name_plural = "seats"
+        unique_together = ["match", "seat_number"]
+
+    def __str__(self):
+        return f"{self.match.id}:{self.seat_number}"
