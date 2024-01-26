f16e99586e732acd4919214893a7d4aaeb76549f
Mohamadhosein Moazennia <mhmoazennia@gmail.com>
2024-01-26 21:38:56 +0330

    feat: Create Reservation model

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    match = models.ForeignKey("matches.Match", on_delete=models.CASCADE)
+    seat = models.ForeignKey("matches.Seat", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    class Meta:
+        verbose_name = "reservation"
+        verbose_name_plural = "reservations"
+
+    def __str__(self):
+        return f"{self.user}:{self.match}:{self.seat}"
