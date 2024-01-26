2496040f81ab9a582258a425c4770a7149650d64
Mohamadhosein Moazennia <mhmoazennia@gmail.com>
2024-01-26 23:46:58 +0330

    doc: Add docs to stadiums views and serializers

--- stadiums/serializers.py
+++ stadiums/serializers.py
@@ -1,14 +1,38 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
 from stadiums.models import Stadium
 
 
 class StadiumSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Stadium model.
+
+    ---
+    # Fields
+    - `id`: The unique identifier for the stadium.
+    - `name`: The name of the stadium.
+    - `location`: The location of the stadium.
+
+    # Validations
+    - A stadium with the same name and location should not already exist.
+    """
+
     class Meta:
         model = Stadium
         fields = ["id", "name", "location"]
 
     def validate(self, data):
+        """
+        Validate that a stadium with the same name and location does not already exist.
+
+        :param data: The data to be validated.
+        :type data: dict
+        :raises serializers.ValidationError: If validation fails.
+        :return: The validated data.
+        :rtype: dict
+        """
         name = data.get("name")
         location = data.get("location")
 
--- stadiums/views.py
+++ stadiums/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.request import Request
@@ -8,10 +10,49 @@ from stadiums.serializers import StadiumSerializer
 
 
 class AddStadiumView(APIView):
+    """
+    View for adding a new stadium.
+
+    ---
+    # Permissions
+    - User must be authenticated.
+    - User must be an admin.
+
+    # Request Body
+    - `name`: The name of the stadium.
+    - `location`: The location of the stadium.
+
+    # Responses
+    - 201 Created: Stadium created successfully.
+    - 400 Bad Request: Invalid request data.
+    """
+
     permission_classes = [IsAuthenticated, IsAdminUser]
     serializer_class = StadiumSerializer
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "name": openapi.Schema(type=openapi.TYPE_STRING),
+                "location": openapi.Schema(type=openapi.TYPE_STRING),
+            },
+            required=["name", "location"],
+        ),
+        responses={
+            201: "Stadium created successfully.",
+            400: "Bad Request. Invalid request data.",
+        },
+    )
     def post(self, request: Request):
+        """
+        Create a new stadium.
+
+        :param request: The HTTP request object.
+        :type request: Request
+        :return: The HTTP response object.
+        :rtype: Response
+        """
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
