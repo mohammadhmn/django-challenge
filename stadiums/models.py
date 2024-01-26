2281b638a68aefd6a7cfbe1f96b6c6f229a0db15
Mohamadhosein Moazennia <mhmoazennia@gmail.com>
2024-01-26 15:39:37 +0330

    feat: Add Stadium model

--- stadiums/models.py
+++ stadiums/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Stadium(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "stadium"
+        verbose_name_plural = "stadiums"
+        unique_together = ["name", "location"]
+
+    def __str__(self):
+        return self.name
