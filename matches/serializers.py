20c1215d4ec4aa8a53d79ad5f5357236e4c4f12a
Mohamadhosein Moazennia <mhmoazennia@gmail.com>
2024-01-26 23:45:42 +0330

    doc: Add docs to matches views and serializers

--- matches/serializers.py
+++ matches/serializers.py
@@ -1,11 +1,31 @@
 from django.core.exceptions import ValidationError
 from django.db.models import Q
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
 from matches.models import Match, Seat
 
 
 class MatchSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Match model.
+
+    ---
+    # Fields
+    - `id`: The unique identifier for the match.
+    - `stadium`: The stadium where the match is held.
+    - `home_side`: The home side participating in the match.
+    - `away_side`: The away side participating in the match.
+    - `match_day`: The day on which the match takes place.
+    - `match_time`: The time at which the match starts.
+
+    # Validations
+    - Home and away sides should be different.
+    - Check if home or away sides have a match on the selected match day and time.
+    - Check if the stadium is busy on the selected match day and time.
+    """
+
     class Meta:
         model = Match
         fields = ["id", "stadium", "home_side", "away_side", "match_day", "match_time"]
@@ -16,6 +36,13 @@ class MatchSerializer(serializers.ModelSerializer):
         return super().validate(data)
 
     def _validate_sides(self, data):
+        """
+        Validate that home and away sides are different and not already occupied.
+
+        :param data: The data to be validated.
+        :type data: dict
+        :raises ValidationError: If validation fails.
+        """
         home_side = data.get("home_side")
         away_side = data.get("away_side")
         match_day = data.get("match_day")
@@ -38,6 +65,13 @@ class MatchSerializer(serializers.ModelSerializer):
             )
 
     def _validate_stadium(self, data):
+        """
+        Validate that the stadium is not busy on the selected match day and time.
+
+        :param data: The data to be validated.
+        :type data: dict
+        :raises ValidationError: If validation fails.
+        """
         match_day = data.get("match_day")
         match_time = data.get("match_time")
         stadium = data.get("stadium")
@@ -52,6 +86,17 @@ class MatchSerializer(serializers.ModelSerializer):
 
 
 class SeatSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Seat model.
+
+    ---
+    # Fields
+    - `id`: The unique identifier for the seat.
+    - `match`: The match to which the seat belongs.
+    - `seat_number`: The seat number.
+    - `is_reserved`: Indicates whether the seat is reserved.
+    """
+
     class Meta:
         model = Seat
         fields = ["id", "match", "seat_number", "is_reserved"]
--- matches/views.py
+++ matches/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.request import Request
@@ -9,11 +11,30 @@ from matches.serializers import MatchSerializer, SeatSerializer
 
 
 class BaseMatchView(APIView):
+    """
+    Base class for Match-related views.
+
+    ---
+    # Permissions
+    - User must be authenticated.
+    - User must be an admin.
+    """
+
     permission_classes = [IsAuthenticated, IsAdminUser]
     serializer_class = None
     model_class = None
 
     def _get_object_or_404(self, pk: int, error: str):
+        """
+        Get an object by its primary key or return a 404 response if not found.
+
+        :param pk: Primary key of the object.
+        :type pk: int
+        :param error: Error message for the 404 response.
+        :type error: str
+        :return: A tuple containing the object and a 404 response or None.
+        :rtype: tuple[object, Response]
+        """
         try:
             object = self.model_class.objects.get(pk=pk)
         except Match.DoesNotExist:
@@ -25,14 +46,64 @@ class BaseMatchView(APIView):
         return object, None
 
     def _create_response(self, data: dict, status_code: int):
+        """
+        Create a custom response.
+
+        :param data: Response data.
+        :type data: dict
+        :param status_code: HTTP status code.
+        :type status_code: int
+        :return: The custom response.
+        :rtype: Response
+        """
         return Response(data=data, status=status_code)
 
 
 class AddMatchView(BaseMatchView):
+    """
+    View for adding a new Match.
+
+    # Request Body
+    - `stadium`: The stadium where the match will be held.
+    - `home_side`: The home side participating in the match.
+    - `away_side`: The away side participating in the match.
+    - `match_day`: The day on which the match will take place.
+    - `match_time`: The time at which the match will start.
+
+    # Responses
+    - 201 Created: Match created successfully.
+    - 400 Bad Request: Invalid request data.
+    """
+
     serializer_class = MatchSerializer
     model_class = Match
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "stadium": openapi.Schema(type=openapi.TYPE_STRING),
+                "home_side": openapi.Schema(type=openapi.TYPE_STRING),
+                "away_side": openapi.Schema(type=openapi.TYPE_STRING),
+                "match_day": openapi.Schema(type=openapi.TYPE_STRING),
+                "match_time": openapi.Schema(type=openapi.TYPE_STRING),
+            },
+            required=["stadium", "home_side", "away_side", "match_day", "match_time"],
+        ),
+        responses={
+            201: "Match created successfully.",
+            400: "Bad Request. Invalid request data.",
+        },
+    )
     def post(self, request: Request):
+        """
+        Create a new Match.
+
+        :param request: The HTTP request object.
+        :type request: Request
+        :return: The HTTP response object.
+        :rtype: Response
+        """
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         match = serializer.save()
@@ -43,10 +114,46 @@ class AddMatchView(BaseMatchView):
 
 
 class AddMatchSeatsView(BaseMatchView):
+    """
+    View for adding seats to a Match.
+
+    # Request Body
+    - `seats`: List of seat data.
+
+    # Responses
+    - 201 Created: Seats created successfully.
+    - 400 Bad Request: Invalid request data or match not found.
+    """
+
     serializer_class = SeatSerializer
     model_class = Match
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "seats": openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Schema(type=openapi.TYPE_OBJECT),
+                )
+            },
+        ),
+        responses={
+            201: "Seats created successfully.",
+            400: "Bad Request. Invalid request data or match not found.",
+        },
+    )
     def post(self, request: Request, match_id: int):
+        """
+        Add seats to a Match.
+
+        :param request: The HTTP request object.
+        :type request: Request
+        :param match_id: The ID of the Match.
+        :type match_id: int
+        :return: The HTTP response object.
+        :rtype: Response
+        """
         match, response = self._get_object_or_404(pk=match_id, error="Match not found")
         if response:
             return response
